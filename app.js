d107d6af890ca0aeb4e631f3e80feefa950d7652
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-07-27 16:32:56 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var nome = "Maria"
+var notaPrimeiroBimestre = 9.5
+var notaSegundoBimestre = 8.3
+var notaTerceiroBimestre = 7.4
+var notaQuartoBimestre = 6.9
+
+var notaFinal = ((notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4).toFixed(2);
+
+console.log(notaFinal);
